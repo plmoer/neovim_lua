f6dd945817f3a3d926b9735a03844d63d30f33d6
Linmin Pei <plmoer@gmail.com>
2021-12-30 15:44:18 -0500

    solving the Tab error: add hrsh7th/vim-vsnip in plugin.lua and comment the has_words_before()

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -85,6 +85,7 @@ return packer.startup(function(use)
   use 'hrsh7th/cmp-path'
   use 'hrsh7th/cmp-cmdline'
   use 'hrsh7th/nvim-cmp'
+  use 'hrsh7th/vim-vsnip'
 
   -- luasnip user 
   use 'saadparwaiz1/cmp_luasnip'
